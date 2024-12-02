da71d80dcbf73e5237f4480084f27be5ade50fd0
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 22:18:35 +0100

    importing + using avatar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Link } from "react-router";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
-  return <div className="w-full h-20 bg-neutral-200">Navbar</div>;
+  return (
+    <div className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center px-6 py-8 bg-neutral-200">
+      <Link to="/">
+        <h4 className="font-bold text-2xl">The Tomato</h4>
+      </Link>
+      <div>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
