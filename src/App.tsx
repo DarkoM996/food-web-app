bf2ba88e1b8d2b908da69eb6334d7d887075469c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 22:06:27 +0100

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import { Button } from "@/components/ui/button";
 import { createBrowserRouter, Route, RouterProvider } from "react-router";
 import { createRoutesFromElements } from "react-router";
 import RootLayout from "./layouts/RootLayout";
+import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<RootLayout />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
 );
 
 const App = () => {
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,8 @@
-import Home from "@/components/Home";
 import { Outlet } from "react-router";
-import Sidebar from "./../components/Sidebar";
 
 const RootLayout = () => {
   return (
     <>
-      <Home />
-      <Sidebar />
       <Outlet />
     </>
   );
