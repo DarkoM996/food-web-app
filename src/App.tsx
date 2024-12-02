8af781cc166889f3c1bfff141e18ce91206e6653
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 22:18:49 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
 import { createBrowserRouter, Route, RouterProvider } from "react-router";
 import { createRoutesFromElements } from "react-router";
 import RootLayout from "./layouts/RootLayout";
