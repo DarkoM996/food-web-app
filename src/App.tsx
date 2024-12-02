96948e560dbdf195e3cb9137670ff56e1d8e74cf
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 17:08:14 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-4xl w-full h-screen bg-neutral-950 text-white">
+      Welcome to this React dashboard
+      <Button>Shadcn Button</Button>
+    </div>
+  );
+};
+
+export default App;
