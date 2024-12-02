32c1de84f624ff1aeecb4ca216e2302d1ecaad26
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 22:07:47 +0100

    latest

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Navbar from "@/components/Navbar";
 
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <div>
+      <p>Something</p>
+    </div>
+  );
 };
 
 export default HomePage;
