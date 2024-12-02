7d031ba10aef1d6321211b116fc12ab92c0b7f27
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 18:16:06 +0100

    testing

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,12 @@
 import Home from "@/components/Home";
-import React from "react";
 import { Outlet } from "react-router";
+import Sidebar from "./../components/Sidebar";
 
 const RootLayout = () => {
   return (
     <>
       <Home />
+      <Sidebar />
       <Outlet />
     </>
   );
