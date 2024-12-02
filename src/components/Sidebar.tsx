d3c8a46f57dd90fb496b0ba41b3bfc76d4520f88
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-02 18:18:30 +0100

    latest

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <div className="w-full h-20 bg-neutral-300">Sidebar</div>;
 };
 
 export default Sidebar;
