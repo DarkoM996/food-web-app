1a93c947a69574e337e90e06ad3dd36a881c6cc1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-05 18:19:44 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Root from "./components/Root";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import DashboardHome from "./components/DashboardHome";
+import Settings from "./components/Settings";
+
 const App = () => {
-  return <div>Reworking</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route index element={<DashboardHome />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
