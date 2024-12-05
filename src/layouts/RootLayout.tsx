27daa92ff0e901ab2ebcde65a38955f8f9ea6415
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-05 21:12:00 +0100

    cleanup

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router";
-import Navbar from "@/components/Navbar";
 
 const RootLayout = () => {
   return (
     <>
-      <Navbar />
-      <div>{<Outlet />}</div>
+      <Outlet />
     </>
   );
 };
