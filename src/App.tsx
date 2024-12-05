e13f39aca3e88ef9ec21353736b0194bef70624c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-05 21:21:51 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { Route, Routes } from "react-router";
-import Navbar from "./components/Navbar";
-import Root from "./components/Root";
 import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
-import DashboardHome from "./components/DashboardHome";
-import Settings from "./components/Settings";
 import RootLayout from "./layouts/RootLayout";
 
 const App = () => {
