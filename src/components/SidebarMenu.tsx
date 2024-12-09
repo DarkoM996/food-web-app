0d12577d702d444866a1674a5f7ec22abac0419b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 20:24:35 +0100

    new component added

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,10 +1,12 @@
 import AccountToggle from "./AccountToggle";
+import Search from "./Search";
 
 const SidebarMenu = () => {
   return (
     <aside className="w-full h-screen bg-neutral-200">
       <div className="sticky top-4 h-[calc(100vh-32px-48px)]">
         <AccountToggle />
+        <Search />
       </div>
 
       {/* TODO: Plan toggle */}
