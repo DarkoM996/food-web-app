95f455523e3eeb8c7cd5fd6f341f20481b7d0856
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 20:13:37 +0100

    adjustments

--- src/components/AccountToggle.tsx
+++ src/components/AccountToggle.tsx
@@ -1,7 +1,7 @@
 const AccountToggle = () => {
   return (
     <div className="border-b mb-4 mt-2 pb-4 border-stone-300">
-      <button className="flex items-center w-full relative gap-2 p-1 hover:bg-stone-200 rounded tranisition-colors">
+      <button className="flex items-center w-full relative gap-2 p-1 hover:bg-stone-300 rounded transition-colors">
         <img
           src="https://api.dicebear.com/9.x/adventurer/svg?seed=Aneka"
           alt="avatar"
