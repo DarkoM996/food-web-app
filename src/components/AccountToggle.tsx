95754a9e77ee6b5eccf63307703d87b01f6445e9
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 20:10:10 +0100

    avatar

--- src/components/AccountToggle.tsx
+++ src/components/AccountToggle.tsx
@@ -1,5 +1,15 @@
 const AccountToggle = () => {
-  return <div>AccountToggle</div>;
+  return (
+    <div className="border-b mb-4 mt-2 pb-4 border-stone-300">
+      <button className="flex items-center w-full relative gap-2 p-1 hover:bg-stone-200 rounded tranisition-colors">
+        <img
+          src="https://api.dicebear.com/9.x/adventurer/svg?seed=Aneka"
+          alt="avatar"
+          className="size-8 rounded shrink-0 bg-violet-500 shadow"
+        />
+      </button>
+    </div>
+  );
 };
 
 export default AccountToggle;
