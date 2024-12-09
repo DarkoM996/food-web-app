5a60c33ad88095ef22433601434c6ab78bba36e7
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 20:50:36 +0100

    latest

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -33,7 +33,10 @@ const CommandMenu = ({
         onClick={(e) => e.stopPropagation()}
         className="bg-white rounded-lg shadow-xl border-stone-300 border overflow-hidden w-full max-w-lg mx-auto mt-12"
       >
-        <Command.Input />
+        <Command.Input
+          placeholder="What do you need"
+          className="relative border-b border-stone-300 p-3 text-lg w-full placeholder:text-stone-400 focus:outline-none"
+        />
         <Command.List>
           <Command.Empty>No results found.</Command.Empty>
 
