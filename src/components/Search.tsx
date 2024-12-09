76234660c8a6926969182fe39c7ab05a4937372b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 20:44:41 +0100

    getting some functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,6 +10,10 @@ const Search = () => {
       <div className="bg-stone-300 mb-4 relative rounded-full flex justify-start items-center px-2 py-1.5 text-sm">
         <FiSearch className="mr-2" />
         <input
+          onFocus={(e) => {
+            e.target.blur();
+            setOpen(true);
+          }}
           type="text"
           placeholder="Search"
           className="w-full bg-transparent placeholder:text-primary_grays-500 focus:outline-none"
