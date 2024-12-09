2ddf6dca3098d96883304ca9732010076d79c7fe
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 20:31:34 +0100

    latest

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <>
+      <div className="bg-stone-200 mb-4 relative rounded flex justify-start items-center px-2 py-1.5 text-sm">
+        <FiSearch className="mr-2" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full bg-transparent placeholder:text-primary_grays-500 focus:outline-none"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Search;
