9a20bfa3cd51cfac35e452ed04ac5f3d2a42d9a8
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 22:01:19 +0100

    latest

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -1,5 +1,9 @@
 const Plan = () => {
-  return <div>Plan</div>;
+  return (
+    <div className="flex sticky top-[calc(100vh_-_48px_-_16px)] flex-col h-12 border-t px-2 border-stone-300 justify-end text-xs">
+      Plan
+    </div>
+  );
 };
 
 export default Plan;
