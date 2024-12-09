c8d660d2d6a47207e6a5660536f900a8c89e49fe
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-09 21:26:40 +0100

    initial

--- src/components/RouteSelect.tsx
+++ src/components/RouteSelect.tsx
@@ -1,5 +1,42 @@
+import { FiHome } from "react-icons/fi";
+import { IconType } from "react-icons/lib";
+import {
+  MdFavoriteBorder,
+  MdOutlineList,
+  MdOutlinePerson,
+  MdOutlinePersonAddAlt,
+  MdOutlineRestaurantMenu,
+  MdPayment,
+} from "react-icons/md";
+
 const RouteSelect = () => {
-  return <div>RouteSelect</div>;
+  return (
+    <div className="space-y-2">
+      <Route Icon={FiHome} selected={true} title="Dashboard" />
+      <Route Icon={MdFavoriteBorder} selected={true} title="Favorites" />
+      <Route Icon={MdOutlineRestaurantMenu} selected={true} title="Orders" />
+      <Route Icon={MdOutlineList} selected={true} title="MenuLayout" />
+      <Route Icon={MdPayment} selected={true} title="Booking" />
+      <Route Icon={MdOutlinePerson} selected={true} title="Sign In" />
+      <Route Icon={MdOutlinePersonAddAlt} selected={true} title="Sign Up" />
+    </div>
+  );
 };
 
 export default RouteSelect;
+
+const Route = ({
+  selected,
+  Icon,
+  title,
+}: {
+  selected: boolean;
+  Icon: IconType;
+  title: string;
+}) => {
+  return (
+    <button>
+      <Icon />
+    </button>
+  );
+};
