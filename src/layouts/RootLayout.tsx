1be2c233a2295161cda03a07505d47f982dd2ae3
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 23:37:09 +0100

    rework

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,15 +4,10 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <main className="grid md:grid-cols-[200px,auto] gap-4">
-      <div className="hidden md:flex">
-        <SidebarMenu />
-      </div>
-      <div className="flex flex-col w-full  gap-6">
-        <Navbar />
-        <Outlet />
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 };
 
