1a4d30c419e988c7a17e7fd5276eb03292f33aad
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 18:30:36 +0100

    reworking this

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,14 +1,7 @@
-import Plan from "./Plan";
-import RouteSelect from "./RouteSelect";
-import Search from "./Search";
-
 const SidebarMenu = () => {
   return (
     <aside className="w-full h-screen bg-primary_white-200 ">
-      <div className="sticky top-4 px-2 h-[calc(100vh-32px-48px)]">
-        <Search />
-      </div>
-      <Plan />
+      <h3>The Tomato</h3>
     </aside>
   );
 };
