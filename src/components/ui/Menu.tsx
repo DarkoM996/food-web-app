1fafe6e60305f9392e6a81204ba8fe3ffaa23177
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 19:31:07 +0100

    test

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,67 +1,22 @@
-import {
-  MdDashboard,
-  MdFavorite,
-  MdMenu,
-  MdPayment,
-  MdPerson,
-  MdRestaurantMenu,
-  MdSettings,
-} from "react-icons/md";
-import { Link } from "react-router";
+// import {
+//   MdDashboard,
+//   MdFavorite,
+//   MdMenu,
+//   MdPayment,
+//   MdPerson,
+//   MdRestaurantMenu,
+//   MdSettings,
+// } from "react-icons/md";
+// import { Link } from "react-router";
 
 const Menu = () => {
   return (
-    <div className="w-full h-full">
-      <div className="flex flex-col justify-start items-start gap-6 w-full h-fit px-3 py-8">
-        <Link
-          to="/"
-          className="flex flex-row justify-start items-center gap-2 p-2 rounded-full bg-neutral-200 w-full"
-        >
-          <MdDashboard size={24} />
-          <p className="text-base">Dashboard</p>
-        </Link>
-        <Link
-          to="/menu"
-          className="flex flex-row justify-start items-center gap-2"
-        >
-          <MdRestaurantMenu size={24} />
-          <p className="text-base">Menu</p>
-        </Link>
-        <Link
-          to="/menu"
-          className="flex flex-row justify-start items-center gap-2"
-        >
-          <MdFavorite size={24} />
-          <p className="text-base">Favorites</p>
-        </Link>
-        <Link
-          to="/menu"
-          className="flex flex-row justify-start items-center gap-2"
-        >
-          <MdMenu size={24} />
-          <p className="text-base">Orders</p>
-        </Link>
-        <Link
-          to="/menu"
-          className="flex flex-row justify-start items-center gap-2"
-        >
-          <MdPayment size={24} />
-          <p className="text-base">Booking</p>
-        </Link>
-        <Link
-          to="/menu"
-          className="flex flex-row justify-start items-center gap-2"
-        >
-          <MdSettings size={24} />
-          <p className="text-base">Settings</p>
-        </Link>
-        <Link
-          to="/menu"
-          className="flex flex-row justify-start items-center gap-2"
-        >
-          <MdPerson size={24} />
-          <p className="text-base">Profile</p>
-        </Link>
+    <div className="w-full h-full flex flex-col">
+      <div className="flex flex-col justify-start items-center gap-2 w-full h-2/3 bg-slate-200">
+        1
+      </div>
+      <div className="flex flex-col justify-start items-center gap-2 w-full h-1/3">
+        2
       </div>
     </div>
   );
