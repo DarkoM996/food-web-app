9ab169b72706d42c47c17da5f2a262305796c13f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 18:49:18 +0100

    new component

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import Menu from "./ui/Menu";
 const SidebarMenu = () => {
   return (
     <aside className="w-full h-screen bg-white ">
@@ -8,6 +9,7 @@ const SidebarMenu = () => {
       >
         <h3 className="font-bold text-2xl">The Tomato</h3>
       </Link>
+      <Menu />
     </aside>
   );
 };
