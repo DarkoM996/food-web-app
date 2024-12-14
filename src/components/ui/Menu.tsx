412f199749a11aff739f8f519611204dc1a3872c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 19:13:54 +0100

    latest

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,4 +1,10 @@
-import { MdDashboard } from "react-icons/md";
+import {
+  MdDashboard,
+  MdFavorite,
+  MdMenu,
+  MdPayment,
+  MdRestaurantMenu,
+} from "react-icons/md";
 import { Link } from "react-router";
 
 const Menu = () => {
@@ -8,6 +14,41 @@ const Menu = () => {
         <Link to="/" className="flex flex-row justify-start items-center gap-2">
           <MdDashboard size={24} />
           <p className="text-base">Dashboard</p>
+        </Link>
+        <Link
+          to="/menu"
+          className="flex flex-row justify-start items-center gap-2"
+        >
+          <MdRestaurantMenu size={24} />
+          <p className="text-base">Menu</p>
+        </Link>
+        <Link
+          to="/menu"
+          className="flex flex-row justify-start items-center gap-2"
+        >
+          <MdFavorite size={24} />
+          <p className="text-base">Favorites</p>
+        </Link>
+        <Link
+          to="/menu"
+          className="flex flex-row justify-start items-center gap-2"
+        >
+          <MdMenu size={24} />
+          <p className="text-base">Orders</p>
+        </Link>
+        <Link
+          to="/menu"
+          className="flex flex-row justify-start items-center gap-2"
+        >
+          <MdPayment size={24} />
+          <p className="text-base">Booking</p>
+        </Link>
+        <Link
+          to="/menu"
+          className="flex flex-row justify-start items-center gap-2"
+        >
+          <MdMenu size={24} />
+          <p className="text-base">Menu</p>
         </Link>
       </div>
     </div>
