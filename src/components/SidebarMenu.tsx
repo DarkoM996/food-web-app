e6d262df498541374c6f13349d060585f028ba70
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 18:42:06 +0100

    design color adjustments for now

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router";
 const SidebarMenu = () => {
   return (
-    <aside className="w-full h-screen bg-neutral-100 ">
+    <aside className="w-full h-screen bg-white ">
       <Link
         to="/"
-        className="flex flex-col justify-center items-center p-4 bg-neutral-50"
+        className="flex flex-col justify-center items-center p-4 border-b border-neutral-100"
       >
         <h3 className="font-bold text-2xl">The Tomato</h3>
       </Link>
