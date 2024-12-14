a7fd24af2ea79cb6589bcb0a7e8eeb2cb3681d5f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 18:37:10 +0100

    color will be reworked later

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router";
 const SidebarMenu = () => {
   return (
-    <aside className="w-full h-screen bg-primary_white-200 ">
-      <h3>The Tomato</h3>
+    <aside className="w-full h-screen bg-neutral-100 ">
+      <Link
+        to="/"
+        className="flex flex-col justify-center items-center p-4 bg-neutral-50"
+      >
+        <h3 className="font-bold text-2xl">The Tomato</h3>
+      </Link>
     </aside>
   );
 };
