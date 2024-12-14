fae82335d7f581608cfafff8897d35759af3e06a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-14 19:09:58 +0100

    initial

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,5 +1,17 @@
+import { MdDashboard } from "react-icons/md";
+import { Link } from "react-router";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div>
+      <div>
+        <Link to="/" className="flex flex-row justify-start items-center gap-2">
+          <MdDashboard size={24} />
+          <p className="text-base">Dashboard</p>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
