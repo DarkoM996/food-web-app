1d11ce730f01f110569200c9aef43a9689295bc2
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 22:40:32 +0100

    icons sorted out

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -12,11 +12,12 @@ import {
 import { Link } from "react-router";
 import {
   LayoutDashboard,
-  Newspaper,
-  Folders,
+  Logs,
+  Heart,
   CreditCard,
   Settings,
   User,
+  Columns,
 } from "lucide-react";
 
 const SidebarMenu = () => {
@@ -31,22 +32,22 @@ const SidebarMenu = () => {
             <Link to="/">Dashboard</Link>
           </CommandItem>
           <CommandItem>
-            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <Logs className="mr-2 h-4 w-4" />
             <Link to="/">Orders</Link>
           </CommandItem>
           <CommandItem>
-            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <Heart className="mr-2 h-4 w-4" />
             <Link to="/">Favorites</Link>
           </CommandItem>
           <CommandItem>
-            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <Columns className="mr-2 h-4 w-4" />
             <Link to="/">Menu Layouts</Link>
           </CommandItem>
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading="Settings">
           <CommandItem>
-            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <User className="mr-2 h-4 w-4" />
             <Link to="/">Profile</Link>
           </CommandItem>
           <CommandItem>
@@ -54,7 +55,7 @@ const SidebarMenu = () => {
             <Link to="/">Booking</Link>
           </CommandItem>
           <CommandItem>
-            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <Settings className="mr-2 h-4 w-4" />
             <Link to="/">Settings</Link>
           </CommandItem>
         </CommandGroup>
