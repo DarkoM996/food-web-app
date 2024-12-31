599eef12815fd5e606df32a684b523656c3963f9
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 23:12:19 +0100

    element imported (error solved)

--- src/components/dashboard/DashboardCard.tsx
+++ src/components/dashboard/DashboardCard.tsx
@@ -1,5 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Newspaper } from "lucide-react";
+
 const DashboardCard = () => {
-  return <div>DashboardCard</div>;
+  return (
+    <Card className="bg-neutral-100 dark:bg-neutral-800 p-4 pb-0">
+      DashboardCard
+      <CardContent />
+    </Card>
+  );
 };
 
 export default DashboardCard;
