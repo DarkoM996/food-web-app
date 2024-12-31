3d3b5a546081e6bc8e36713886f571bedee3ec73
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 22:02:05 +0100

    reworking

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,9 +1,7 @@
-import Menu from "./ui/Menu";
-
 const SidebarMenu = () => {
   return (
     <aside className="w-full min-h-screen sticky top-0 bg-white ">
-      <Menu />
+      something
     </aside>
   );
 };
