511b9e90e106dec5ed9a2958a5d7845db93b27dd
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 23:25:13 +0100

    making the card dynamic with ts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,14 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Newspaper } from "lucide-react";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col md:flex-row justify-between gap-5 my-5">
-      <DashboardCard />
+      <DashboardCard
+        title="Posts"
+        count={100}
+        icon={<Newspaper className="text-neutral-500" size={72} />}
+      />
     </div>
   );
 };
