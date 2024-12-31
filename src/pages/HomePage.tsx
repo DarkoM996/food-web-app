b610f3875c67c1c0a425f908bfaaf19f53f661f1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 23:06:39 +0100

    initial work

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import DashboardCard from "@/components/dashboard/DashboardCard";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="flex flex-col md:flex-row justify-between gap-5 my-5">
       <DashboardCard />
     </div>
   );
