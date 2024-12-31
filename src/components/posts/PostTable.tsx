27c66a3e5a25b558ab50dad45662fd7f26dd7bca
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 00:50:29 +0100

    using ts in react

--- src/components/posts/PostTable.tsx
+++ src/components/posts/PostTable.tsx
@@ -10,8 +10,17 @@ import { Link } from "react-router";
 import posts from "@/data/posts";
 import { Post } from "@/types/posts";
 
-const PostTable = () => {
-  return <div>PostTable</div>;
+interface PostTableProps {
+  limit?: number;
+  title?: string;
+}
+
+const PostTable = ({ limit, title }: PostTableProps) => {
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3>
+    </div>
+  );
 };
 
 export default PostTable;
