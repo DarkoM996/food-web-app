90e81a4c534f9387d50eb887a9ceaf7755a661cd
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 23:17:09 +0100

    latest

--- src/components/dashboard/DashboardCard.tsx
+++ src/components/dashboard/DashboardCard.tsx
@@ -4,8 +4,14 @@ import { Newspaper } from "lucide-react";
 const DashboardCard = () => {
   return (
     <Card className="bg-neutral-100 dark:bg-neutral-800 p-4 pb-0">
-      DashboardCard
-      <CardContent />
+      <CardContent className="">
+        <h3 className="text-3xl text-center mb-4 font-bold text-neutral-800">
+          Posts
+        </h3>
+        <div className="flex gap-5 justify-center items-center">
+          <Newspaper />
+        </div>
+      </CardContent>
     </Card>
   );
 };
