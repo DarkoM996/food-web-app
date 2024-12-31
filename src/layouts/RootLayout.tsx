3939bd15e242db203f1a139896270d8abb07d13b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 23:47:57 +0100

    latest

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -10,7 +10,7 @@ const RootLayout = () => {
         <div className="hidden md:block h-[100vh] w-[300px]">
           <SidebarMenu />
         </div>
-        <div className="p-5 w-full md:max-w-screen-2xl mx-auto">
+        <div className="p-5 w-full md:max-w-screen-xl mx-auto">
           <Outlet />
         </div>
       </div>
