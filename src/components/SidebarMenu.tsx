8d4f265882de333d7fe111bd9581c4a9d7b8d167
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-31 22:05:13 +0100

    new shadcn component added

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,8 +1,43 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { Link } from "react-router";
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from "lucide-react";
+
 const SidebarMenu = () => {
   return (
-    <aside className="w-full min-h-screen sticky top-0 bg-white ">
-      something
-    </aside>
+    <Command>
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>Calendar</CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>Profile</CommandItem>
+          <CommandItem>Billing</CommandItem>
+          <CommandItem>Settings</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
   );
 };
 
