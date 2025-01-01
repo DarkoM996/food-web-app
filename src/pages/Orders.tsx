6a8e0180a9489e2b8f83e72baf6fb86535463988
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 02:28:20 +0100

    new component pagination implemented

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,11 +1,13 @@
 import PostTable from "@/components/posts/PostTable";
 import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
 
 const Orders = () => {
   return (
     <>
       <BackButton text="Go Back" link="/" />
       <PostTable />
+      <PostsPagination />
     </>
   );
 };
