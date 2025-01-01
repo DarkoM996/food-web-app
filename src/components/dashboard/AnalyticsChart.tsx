64f174e21995abe2724741e5b2138fcaf3247540
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 01:56:04 +0100

    latest

--- src/components/dashboard/AnalyticsChart.tsx
+++ src/components/dashboard/AnalyticsChart.tsx
@@ -16,7 +16,26 @@ import {
 import data from "@/data/analytics";
 
 const AnalyticsChart = () => {
-  return <div>AnalyticsChart</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Analytics For This Year</CardTitle>
+        <CardDescription>Orders Per Month</CardDescription>
+        <CardContent>
+          <div style={{ width: "100%", height: 300 }}>
+            <ResponsiveContainer>
+              <LineChart width={1200} height={300} data={data}>
+                <Line type="monotone" dataKey="uv" stroke="#000" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="name" />
+                <YAxis />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </CardContent>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default AnalyticsChart;
