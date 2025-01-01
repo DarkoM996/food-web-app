b1a659c58d154769d332df9ed75bb8bfc66561a3
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 02:29:17 +0100

    adding more stuff (design decision)

--- src/components/posts/PostsPagination.tsx
+++ src/components/posts/PostsPagination.tsx
@@ -19,6 +19,12 @@ const PostsPagination = () => {
           <PaginationLink href="#">1</PaginationLink>
         </PaginationItem>
         <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
         <PaginationItem>
