3e24a3bf5714b364d980c5d44fce265d080fcf00
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 01:39:57 +0100

    testing a different design

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -5,9 +5,11 @@ import { Outlet } from "react-router";
 const RootLayout = () => {
   return (
     <>
-      <Navbar />
+      <div className="px-6 py-2">
+        <Navbar />
+      </div>
       <div className="flex flex-row justify-start gap-2">
-        <div className="hidden md:block h-[100vh] w-[300px]">
+        <div className="hidden md:block h-[100vh] w-[300px] px-6 py-4">
           <SidebarMenu />
         </div>
         <div className="p-5 w-full md:max-w-screen-2xl mx-auto lg:px-16">
