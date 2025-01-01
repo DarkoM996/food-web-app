fae9c7c778ef96c67d06f9495b3259f20c4d1491
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 03:18:05 +0100

    potential fix

--- src/components/posts/PostTable.tsx
+++ src/components/posts/PostTable.tsx
@@ -52,7 +52,7 @@ const PostTable = ({ limit, title }: PostTableProps) => {
                 {post.date}
               </TableCell>
               <TableCell>
-                <Link to={`/orders/edit/${post.id}`}>
+                <Link to={`/orders/${post.id}`}>
                   <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full text-xs">
                     Edit
                   </button>
