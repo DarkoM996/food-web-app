7a68ab0d9879113d4acdffb2c78e92b961a287a7
DarkoM996 <darko.mihajlovski96@gmail.com>
2025-01-01 01:49:48 +0100

    work begins

--- src/components/dashboard/AnalyticsChart.tsx
+++ src/components/dashboard/AnalyticsChart.tsx
@@ -1,3 +1,20 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import data from "@/data/analytics";
+
 const AnalyticsChart = () => {
   return <div>AnalyticsChart</div>;
 };
